f3f3d7242767ffc16e91140d016e88c535bb19bd
minseok <alstjr706@naver.com>
2023-03-14 14:42:04 +0900

    Initial commit

---
+++ src/main/java/com/example/practicemvc/PracticeMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicemvc/PracticeMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
