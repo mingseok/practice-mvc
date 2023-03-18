e3c11434347217b2820eb6f443d50858a66ac03d
minseok <alstjr706@naver.com>
2023-03-18 16:38:18 +0900

    feat : (MVC) Form 전송 객체 분리

--- src/main/java/com/example/practicemvc/domain/item/Item.java
+++ src/main/java/com/example/practicemvc/domain/item/Item.java
@@ -1,36 +1,12 @@
 package com.example.practicemvc.domain.item;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) // 수정할때만
     private Long id;
-
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}) // 전부 체크 한다.
     private String itemName;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class}) // 전부 체크 한다.
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})  // 전부 체크 한다.
     private Integer price;
-
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class}) // 전부 체크 한다.
-    @Max(value = 9999, groups = {SaveCheck.class}) // 저장할때만
     private Integer quantity;
-
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
--- src/main/java/com/example/practicemvc/web/validation/ValidationItemControllerV4.java
+++ src/main/java/com/example/practicemvc/web/validation/ValidationItemControllerV4.java
@@ -3,6 +3,8 @@ package com.example.practicemvc.web.validation;
 import com.example.practicemvc.domain.item.Item;
 import com.example.practicemvc.domain.item.ItemRepository;
 import com.example.practicemvc.domain.item.SaveCheck;
+import com.example.practicemvc.web.validation.form.ItemSaveForm;
+import com.example.practicemvc.web.validation.form.ItemUpdateForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -16,7 +18,7 @@ import java.util.List;
 
 @Slf4j
 @Controller
-@RequestMapping("/validation/v3/items")
+@RequestMapping("/validation/v4/items")
 @RequiredArgsConstructor
 public class ValidationItemControllerV4 {
 
@@ -43,23 +45,32 @@ public class ValidationItemControllerV4 {
         return "validation/v3/addForm";
     }
 
+    // @ModelAttribute("item") ItemSaveForm form -> 괄호는 뭔가? 뷰 탬플릿에서 item으로 사용할거라는 뜻이다.
     @PostMapping("/add")
-    public String addItem(@Validated(SaveCheck.class) @ModelAttribute Item item,
-                          BindingResult bindingResult,
-                          RedirectAttributes redirectAttributes) {
+    public String addItem(@Validated @ModelAttribute("item") ItemSaveForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
-        if (item.getPrice() != null && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
 
         //성공 로직
+        /**
+         * 이렇게 하는 이유는 itemRepository.save()에 들어가보면 Item 타입으로만 들어가야 된다.
+         * 그렇기에 Item 객체를 생성해서, form 게터를 사용해서 저장된 데이터들을 꺼내
+         * item 클래스의 세터로 저장시키는 것이다. 그 다음 item객체를 save에 저장 시킨다.
+         */
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setPrice(form.getPrice());
+        item.setQuantity(form.getQuantity());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
-        return "redirect:/validation/v3/items/{itemId}";
+        return "redirect:/validation/v4/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
@@ -70,16 +81,26 @@ public class ValidationItemControllerV4 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute("item") ItemUpdateForm form, BindingResult bindingResult) {
 
-        if (item.getPrice() != null && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
+        //특정 필드 예외가 아닌 전체 예외
+        if (form.getPrice() != null && form.getQuantity() != null) {
+            int resultPrice = form.getPrice() * form.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
 
-        itemRepository.update(itemId, item);
-        return "redirect:/validation/v3/items/{itemId}";
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v4/editForm";
+        }
+
+        Item itemParam = new Item();
+        itemParam.setItemName(form.getItemName());
+        itemParam.setPrice(form.getPrice());
+        itemParam.setQuantity(form.getQuantity());
+        itemRepository.update(itemId, itemParam);
+        return "redirect:/validation/v4/items/{itemId}";
     }
 }
